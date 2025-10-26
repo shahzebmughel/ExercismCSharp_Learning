3598901f8e77f9a056b625a600e1d2f801cdfa68
shahzebmughel <shahzebmughel@hotmail.com>
2025-10-26 21:26:29 +0100

    Inital Commit for using Exercism as a learning Plattform

---
+++ csharp/cars-assemble/CarsAssemble.cs
@@ -0,0 +1,20 @@
+static class AssemblyLine
+{
+    public static double SuccessRate(int speed)
+    {
+        if (expr)
+        {
+            
+        }
+    }
+    
+    public static double ProductionRatePerHour(int speed)
+    {
+        throw new NotImplementedException("Please implement the (static) AssemblyLine.ProductionRatePerHour() method");
+    }
+
+    public static int WorkingItemsPerMinute(int speed)
+    {
+        throw new NotImplementedException("Please implement the (static) AssemblyLine.WorkingItemsPerMinute() method");
+    }
+}
