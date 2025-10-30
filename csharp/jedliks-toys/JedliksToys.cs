1f8b840582bc5b377c2976854741252ccc6eb108
shahzebmughel <shahzebmughel@hotmail.com>
2025-10-30 05:43:39 +0100

    task completed

--- csharp/jedliks-toys/JedliksToys.cs
+++ csharp/jedliks-toys/JedliksToys.cs
@@ -1,22 +1,35 @@
+// var car = RemoteControlCar.Buy();
+// for (int i = 0; i < 100; i++)
+// {
+//     Console.WriteLine(car.BatteryDisplay());
+//     Console.WriteLine(car.DistanceDisplay());
+//     car.Drive();
+// }
+
 class RemoteControlCar
 {
+    private int _metersDriven = 0;
+    private int _battery = 100;
+    
     public static RemoteControlCar Buy()
     {
-        throw new NotImplementedException("Please implement the (static) RemoteControlCar.Buy() method");
+        return new RemoteControlCar();
     }
 
     public string DistanceDisplay()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.DistanceDisplay() method");
+        return $"Driven {_metersDriven} meters";
     }
 
     public string BatteryDisplay()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.BatteryDisplay() method");
+        return _battery == 0 ? "Battery empty" : $"Battery at {_battery}%";
     }
 
     public void Drive()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.Drive() method");
+        if (_battery <= 0) return;
+        _metersDriven += 20;
+        _battery -= 1;
     }
 }
